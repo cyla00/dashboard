ea2219693d496231742df876f023cee319889cd8
cyla00 <ikhayam000@protonmail.com>
2020-09-01 16:02:12 +0200

    connection class to database done

---
+++ model/connection.php
@@ -0,0 +1,26 @@
+<?php
+class Connection{
+  private $db_host;
+  private $db_username;
+  private $db_password;
+  private $db_name;
+  private $db_charset;
+
+  public function connect(){
+    $this->db_host = "";
+    $this->db_username = "";
+    $this->db_password = "";
+    $this->db_name = "";
+    $this->db_charset = "utf8";
+
+    try {
+      $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name.';charset='.$this->db_charset.';port=3307', $this->db_username, $this->db_password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected!"; // testing purpouse
+      return $conn;
+    } catch (PDOException $e) {
+      echo "Error!: " . $e->getMessage();
+      die();
+    }
+  }
+}
