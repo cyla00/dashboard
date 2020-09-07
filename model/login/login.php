f3980bc8a07044809e141369f122a4f742aad601
cyla00 <ikhayam000@protonmail.com>
2020-09-07 09:39:36 +0200

    added admin role in db + role check

--- model/login/login.php
+++ model/login/login.php
@@ -19,6 +19,7 @@
     public function check(){
       $reg_conn = new Connection();
       $conn = $reg_conn->connect();
+      session_start();
 
       $log_check = "SELECT user_password FROM user WHERE user_email=:email";
 
@@ -26,13 +27,27 @@
       $log_prep->execute([':email'=>$this->login_email]);
       $dump = $log_prep->fetch(PDO::FETCH_OBJ);
 
+      $_SESSION['logged_email'] = $this->login_email;
+
       $pass_check = password_verify($this->login_password, $dump->user_password);
 
       if(empty($this->login_password)){
         header('Location: index.php?login=empty');
       }
       else if($pass_check == true){
-        header('Location: index.php?login=success');
+        $role_check = "SELECT admin_role FROM user WHERE user_email=:email";
+        $role_prep = $conn->prepare($role_check);
+        $role_prep->execute([':email' => $_SESSION['logged_email']]);
+        $role_dump = $role_prep->fetch(PDO::FETCH_NUM);
+
+        if($role_dump[0] == 'user'){
+          echo "not admin";
+          header('Location: index.php?login=success&user=user');
+        }
+        else{
+          echo "admin";
+          header('Location: index.php?login=success&user=admin');
+        }
       }
       else{
         header('Location: index.php?login=fail');
